9c1056d9bb44e408dd9bb37b979efe06639300b3
Arturo Filio Villa <arturfil@hotmail.com>
2018-12-24 18:25:33 -0600

    adding login and register route

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 6, ErrorMessage = 
+        "You must specifiy password between 6 and 8 characters ")]
         public string Password { get; set; }
     }
 }
